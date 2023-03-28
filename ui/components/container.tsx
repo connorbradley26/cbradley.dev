60199f7174697a91b2a9464ed854fe9fab7f1274
connorbradley26 <107945096+connorbradley26@users.noreply.github.com>
2023-03-28 08:49:07 +0100

    Fixed colour issues on light mode

--- ui/components/container.tsx
+++ ui/components/container.tsx
@@ -5,7 +5,7 @@ const Container = (props: any) => {
         <>
             <div className="fixed inset-0 flex justify-center sm:px-8 -z-10">
                 <div className="flex w-full max-w-7xl lg:px-8">
-                    <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
+                    <div className="w-full  bg-zinc-900 border-l border-r border-zinc-700">
                     </div>
                 </div>
             </div>
